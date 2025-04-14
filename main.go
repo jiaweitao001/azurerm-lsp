608aaf800bcf9351238ce34dde52756077a6556f
hqhqhqhqhqhqhqhqhqhqhqhq <harryqu@microsoft.com>
2025-04-14 10:37:17 +1000

    update

--- main.go
+++ main.go
@@ -39,11 +39,6 @@ func main() {
 	}
 
 	c.Commands = map[string]cli.CommandFactory{
-		"completion": func() (cli.Command, error) {
-			return &cmd.CompletionCommand{
-				Ui: ui,
-			}, nil
-		},
 		"serve": func() (cli.Command, error) {
 			return &cmd.ServeCommand{
 				Ui:      ui,
